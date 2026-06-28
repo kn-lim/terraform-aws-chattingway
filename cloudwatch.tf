b22f35fee2c098530fd2bb4b091be50df00b3b9f
Kevin Lim <knlim95@gmail.com>
2026-06-27 22:04:57 -0700

    feat!: remove unnecessary files due to modules

--- cloudwatch.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "endpoint" {
-  name              = "/aws/lambda/${var.name}-endpoint"
-  retention_in_days = var.retention_in_days
-}
-
-resource "aws_cloudwatch_log_group" "task" {
-  name              = "/aws/lambda/${var.name}-task"
-  retention_in_days = var.retention_in_days
-}
